8ead2fc616fed4cd92de4f1e72478cf64ebaff76
mavihS-0 <shivam9753singh@gmail.com>
2024-03-14 21:18:08 +0530

    finalizing commit to jdbc template

--- database-jdbc-template-from-scratch/src/main/java/com/mavcorp/database/DatabaseApplication.java
+++ database-jdbc-template-from-scratch/src/main/java/com/mavcorp/database/DatabaseApplication.java
@@ -25,5 +25,6 @@ public class DatabaseApplication implements CommandLineRunner {
 		log.info("Datasource: "+dataSource.toString());
 		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
 		restTemplate.execute("select 1");
+		
 	}
 }
